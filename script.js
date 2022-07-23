b8ba54f12d0c9171d4c61dd5c410c89f8db1ca31
Hermes <hermesd.guerrero@gmail.com>
2022-07-23 03:43:21 -0500

    Further improve object creation

--- script.js
+++ script.js
@@ -1,22 +1,4 @@
-// Setting up base information for the database
-// let shuttles = [
-//     ["Saturn V", "USA", "Kerosene", 1960, 1980],
-//     ["Space Shuttle", "USA", "Hydrogen", 1980, 2011],
-//     ["Ariane 5", "Europe", "Hydrogen", 1960, "Actualidad"],
-// ];
-
-// let manned = [
-//     ["Dragon V2", "USA", 4, 2012, "Actualidad"],
-//     ["Apollo", "USA", 3, 1960, 1980],
-//     ["Soyuz", "Russia", 3, 1960, "Actualidad"],
-// ];
-
-// let unmanned = [
-//     ["Voyager 2", "USA", "Interstellar space", 1960, "Actualidad"],
-//     ["New Horizons", "USA", "Pluto", 2005, 2018],
-//     ["Cassini-Huygens", "Europe", "Saturn", 2005, 2017],
-// ];
-
+//Pre-existed data used by the program
 let spacecrafts = {
     shuttles: {
         0: {
@@ -91,11 +73,8 @@ let spacecrafts = {
 
 console.log(spacecrafts);
 
-// Program starts
+// Program starts and asks for user input
 function main(){
-    // console.table(shuttles);
-    // console.table(manned);
-    // console.table(unmanned);
 
     const action = prompt("Seleccione una opcion: \n1. Agregar\n2. Buscar ")
 
@@ -111,45 +90,45 @@ function main(){
 }
 
 
+//Function to add data to the pre-existing data
 function addData(){
+    
     const spaceCraftType = prompt("Seleccion el tipo de nave: \n1. Transportador\n2. Tripulada\n3. No Tripulada");
 
+        //If shuttle is selected
         if (spaceCraftType == 1) {
-            const shuttleName = prompt("Ingrese el nombre de la nave: ");
-            const shuttleCountry = prompt("Ingrese el pais de fabricacion: ");
-            const shuttleCombustible = prompt("Ingrese el tipo de combustible: ");
-            const shuttleStartYear = prompt("Ingrese el año de fabricacion: ");
-            const shuttleEndYear = prompt("Ingrese el año de retiro de la nave (Ingrese '0' si aun esta operativa): ");
             // Data gets added to database
             spacecrafts.shuttles[parseInt(Object.keys(spacecrafts.shuttles).pop()) + 1]= {
-                name: shuttleName,
-                country: shuttleCountry,
-                combustible: shuttleCombustible,
-                startYear: shuttleStartYear,
-                endYear: shuttleEndYear
+                name: prompt("Ingrese el nombre de la nave: "),
+                country: prompt("Ingrese el pais de fabricacion: "),
+                combustible: prompt("Ingrese el tipo de combustible: "),
+                startYear: prompt("Ingrese el año de fabricacion: "),
+                endYear: prompt("Ingrese el año de retiro de la nave (Ingrese '0' si aun esta operativa): ")
             }
             alert("Datos agregados satisfactoriamente!");
             main();
 
+        //If manned is selected
         } else if (spaceCraftType == 2) {
-            const mannedName = prompt("Ingrese el nombre de la nave: ");
-            const mannedCountry = prompt("Ingrese el pais de fabricacion: ");
-            const mannedMaxCrew = prompt("Ingrese la cantidad maxima de la tripulacion: ");
-            const mannedStartYear = prompt("Ingrese el año de fabricacion: ");
-            const mannedEndYear = prompt("Ingrese el año de retiro de la nave (Ingrese '0' si aun esta operativa): ");
-            // Data gets added to database
-            manned.push([mannedName, mannedCountry, mannedMaxCrew, mannedStartYear, mannedEndYear]);
+            spacecrafts.manned[parseInt(Object.keys(spacecrafts.manned).pop()) + 1]= {
+                name: prompt("Ingrese el nombre de la nave: "),
+                country: prompt("Ingrese el pais de fabricacion: "),
+                maxCrew: prompt("Ingrese el numero maximo de tripulantes: "),
+                startYear: prompt("Ingrese el año de fabricacion: "),
+                endYear: prompt("Ingrese el año de retiro de la nave (Ingrese '0' si aun esta operativa): ")
+            }
             alert("Datos agregados satisfactoriamente!");
             main();
 
+        //If unmanned is selected
         } else if (spaceCraftType == 3) {
-            const unmannedName = prompt("Ingrese el nombre de la nave: ");
-            const unmannedCountry = prompt("Ingrese el pais de fabricacion: ");
-            const unmannedObjectStudy = prompt("Ingrese el tipo de combustible: ");
-            const unmannedStartYear = prompt("Ingrese el año de fabricacion: ");
-            const unmannedEndYear = prompt("Ingrese el año de retiro de la nave (Ingrese '0' si aun esta operativa): ");
-            // Data gets added to database
-            unmanned.push([unmannedName, unmannedCountry, unmannedObjectStudy, unmannedStartYear, unmannedEndYear]);
+            spacecrafts.unmanned[parseInt(Object.keys(spacecrafts.unmanned).pop()) + 1]= {
+                name: prompt("Ingrese el nombre de la nave: "),
+                country: prompt("Ingrese el pais de fabricacion: "),
+                objectStudy: prompt("Ingrese el objetivo de estudio: "),
+                startYear: prompt("Ingrese el año de fabricacion: "),
+                endYear: prompt("Ingrese el año de retiro de la nave (Ingrese '0' si aun esta operativa): ")
+            }
             alert("Datos agregados satisfactoriamente!");
             main();
 
